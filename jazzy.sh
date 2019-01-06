ca646b569a35a4fa5ca04705f5c8f5a5d344f2ec
zonble <zonble@gmail.com>
2019-01-07 00:51:36 +0800

    1.1.11.

--- jazzy.sh
+++ jazzy.sh
@@ -3,8 +3,8 @@ jazzy \
   --author KKBOX \
   --author_url https://www.kkbox.com \
   --github_url https://github.com/kkbox/OpenAPI-ObjectiveC \
-  --github-file-prefix https://github.com/kkbox/OpenAPI-ObjectiveC/tree/1.1.10 \
-  --module-version 1.1.10 \
+  --github-file-prefix https://github.com/kkbox/OpenAPI-ObjectiveC/tree/1.1.11 \
+  --module-version 1.1.11 \
   --umbrella-header KKBOXOpenAPI/KKBOXOpenAPI.h \
   --framework-root . \
   --module KKBOXOpenAPI
